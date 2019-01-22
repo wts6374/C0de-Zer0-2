390804b12e30447e84d9d3d876f07a66a4dbd805
ejp4604 <ejp4604@g.rit.edu>
2019-01-21 21:33:57 -0500

    Added GUI Manager script and GameOver scene. Game loads game over scene when moves reach 0. Left clicking mouse currently decrements moves (no specific precedent set yet). Created prefabs folder.
    **NOTE**
    I have added a .gitignore that should help with staging, commiting, and pushing stuff to the repo.

---
+++ Connect3/Assets/Scripts/ElliotScripts/MyGUIManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGUIManager : MonoBehaviour {
+
+    MySceneManager sceneManager;
+
+    private void Start()
+    {
+        sceneManager = this.GetComponent<MySceneManager>();
+    }
+
+    private void OnGUI()
+    {
+        GUI.TextArea(new Rect(10, 10, 100, 40), "Score: " + sceneManager.score + "\nMoves: " + sceneManager.numberOfMoves);
+    }
+}
