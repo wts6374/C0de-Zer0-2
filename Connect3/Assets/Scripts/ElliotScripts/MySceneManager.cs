b4654c67258fe163591e09ef5cd51ede0223fdf7
ejp4604 <ejp4604@g.rit.edu>
2019-01-21 21:26:53 -0500

    Project Fixed Added Base scene managing script

--- Connect3/Assets/Scripts/ElliotScripts/MySceneManager.cs
+++ Connect3/Assets/Scripts/ElliotScripts/MySceneManager.cs
@@ -1,16 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MySceneManager : MonoBehaviour {
 
+    public int numberOfMoves;
+    public float score;
 	// Use this for initialization
 	void Start () {
-		
+        numberOfMoves = 5;
+        score = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetMouseButtonDown(0))
+            numberOfMoves--;
+        if (numberOfMoves == 0)
+            EndGame();
 	}
+
+    void EndGame()
+    {
+
+    }
 }
