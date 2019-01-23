981f7c9e53ff9d987b4886928c5c389a5f44ced6
wts6374 <wts6374@g.rit.edu>
2019-01-23 18:27:05 -0500

    Ball physics and example

---
+++ Connect3/Assets/Scripts/BallFall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallFall : MonoBehaviour {
+
+    // Use this for initialization
+    public bool trigger;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
