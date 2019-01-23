2f18c7936251e21c63f596b35b4a13a61a980df2
ejp4604 <ejp4604@g.rit.edu>
2019-01-23 16:45:59 -0500

    Chips script created to help identify each chip whether it red or blue.  Chips have SetChip function that takes 1 or 0 and a material. This function adds an integer identifier value and a material that is red based on the number being 1 or blue based on the number being 0.
    Added Chip prefab to prefabs folder.  Used in MySceneManager to instantiate new chips.
    
    Added starter code that creates 5 default chips in a row to demonstrate that the chip set up that I've created works properly.  Uses random number generation of either 0 or 1 to assign values and materials to the correct chips being created.

---
+++ Connect3/Assets/Scripts/ElliotScripts/cone3t Scripts/Chips.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chips : MonoBehaviour {
+
+    public int chipNumCode;
+
+    public void SetChip(int num, Material mat)
+    {
+        this.GetComponent<Renderer>().material = mat;
+        chipNumCode = num;
+    }
+}
--- Connect3/Assets/Scripts/ElliotScripts/cone3t Scripts/MySceneManager.cs
+++ Connect3/Assets/Scripts/ElliotScripts/cone3t Scripts/MySceneManager.cs
@@ -7,10 +7,39 @@ public class MySceneManager : MonoBehaviour {
 
     public int numberOfMoves;
     public float score;
+    public Material redMat;
+    public Material blueMat;
+    public GameObject blankChip;
+    public List<GameObject> chips;
+
 	// Use this for initialization
 	void Start () {
+        chips = new List<GameObject>();
         numberOfMoves = 5;
         score = 0;
+
+        // REMOVE LATER
+        // value used to place chips next to each other for testing purposes
+        int chipPlacement = 0;
+
+        // creates 5 chips by default 
+        for(int x = 0; x < 5; x++)
+        {
+            // randomly chooses either 1 or 2
+            int randomNum = Random.Range(0, 2);
+
+            // if 1; sets Chip identifier to 1 and gives it the red material
+            if(randomNum == 1)
+                blankChip.GetComponent<Chips>().SetChip(1, redMat);
+            else // if 2; sets Chip identifier to 0 and gives it the blue material
+                blankChip.GetComponent<Chips>().SetChip(0, blueMat);
+
+            // instantiates Chip into scene
+            chips.Add(Instantiate(blankChip, new Vector3(chipPlacement, 1, 1), Quaternion.identity));
+            chipPlacement += 1;
+        }
+            
+
 	}
 	
 	// Update is called once per frame
