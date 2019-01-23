6bdf293fba38752f4c339484e81d48d38b84f869
ebd1998 <ejp4604@rit.edu>
2019-01-23 09:52:34 -0500

    Started making timer and combo logic

---
+++ Connect3/Assets/Scripts/ElliotScripts/cone3t Scripts/comboTimer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class comboTimer : MonoBehaviour {
+
+    bool comboing;
+    float comboEndTime;
+
+	// Use this for initialization
+	void Start () {
+        comboing = false;
+        comboEndTime = 20.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+       
+
+        if(comboing == true)
+        {
+            comboEndTime -= Time.deltaTime;
+            //if() // determines whether combo is occuring or not
+            {
+                //comboEndTime += added time from continuous combo
+            }
+            if (comboEndTime <= 0.0f)
+            {
+                comboing = false;
+                comboEndTime = 20.0f;
+            }
+        }
+        //else if() // determines whether combo is occuring or not
+        {
+            comboing = true;
+        }
+
+
+    }
+}
