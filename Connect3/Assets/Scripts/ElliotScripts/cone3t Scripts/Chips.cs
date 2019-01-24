59b9bc8b17edfc7f4619159df7c3cf1af9582273
ejp4604 <ejp4604@g.rit.edu>
2019-01-23 22:30:28 -0500

    Added some code to handle switching of chips.  Currently works as switching of any chips in any location.  Also switches instantly. FUTURE PLANS: - Only switch with chips in the immediate cardinal directions - Have chips ease into the switch

--- Connect3/Assets/Scripts/ElliotScripts/cone3t Scripts/Chips.cs
+++ Connect3/Assets/Scripts/ElliotScripts/cone3t Scripts/Chips.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 public class Chips : MonoBehaviour {
 
     public int chipNumCode;
+    public int index;
 
     public void SetChip(int num, Material mat)
     {
         this.GetComponent<Renderer>().material = mat;
         chipNumCode = num;
+    }
+
+    private void OnMouseDown()
+    {
+        GameObject[] localSwitch = GameObject.Find("MySceneManager").GetComponent<MySceneManager>().switchingArray;
+
+        if (Input.GetMouseButtonDown(0) && localSwitch[1] == null)
+        {
+            if (localSwitch[0] == null)
+                localSwitch[0] = this.gameObject;
+            else
+                localSwitch[1] = this.gameObject;
+        }
     }
 }
--- Connect3/Assets/Scripts/ElliotScripts/cone3t Scripts/MySceneManager.cs
+++ Connect3/Assets/Scripts/ElliotScripts/cone3t Scripts/MySceneManager.cs
@@ -11,12 +11,16 @@ public class MySceneManager : MonoBehaviour {
     public Material blueMat;
     public GameObject blankChip;
     public List<GameObject> chips;
+    public GameObject[] switchingArray;
 
 	// Use this for initialization
 	void Start () {
         chips = new List<GameObject>();
         numberOfMoves = 5;
         score = 0;
+
+        switchingArray = new GameObject[2];
+        
 
         // REMOVE LATER
         // value used to place chips next to each other for testing purposes
@@ -34,6 +38,8 @@ public class MySceneManager : MonoBehaviour {
             else // if 2; sets Chip identifier to 0 and gives it the blue material
                 blankChip.GetComponent<Chips>().SetChip(0, blueMat);
 
+            blankChip.GetComponent<Chips>().index = x;
+
             // instantiates Chip into scene
             chips.Add(Instantiate(blankChip, new Vector3(chipPlacement, 1, 1), Quaternion.identity));
             chipPlacement += 1;
@@ -48,10 +54,37 @@ public class MySceneManager : MonoBehaviour {
             numberOfMoves--;
         if (numberOfMoves == 0)
             EndGame();
+
+        HandleSwitching();
 	}
 
     void EndGame()
     {
         SceneManager.LoadSceneAsync(2);
+    }
+
+    void HandleSwitching()
+    {
+        if(switchingArray[1] != null)
+        {
+            Debug.Log("INSIDE");
+            //if(switchingArray[0].transform.position.x - switchingArray[1].transform.position.x < 0)
+            //{
+
+            //}
+
+            Vector3 pos1 = switchingArray[0].transform.position;
+            Vector3 pos2 = switchingArray[1].transform.position;
+
+            switchingArray[0].transform.position = pos2;
+            switchingArray[1].transform.position = pos1;
+
+            for (int x = 0; x < 2; x++)
+                switchingArray[x] = null;
+        }
+
+
+
+        
     }
 }
