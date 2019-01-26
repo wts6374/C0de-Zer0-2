149352d7e1abf58e19b1cb939d8f14f61e27f21d
Patrick Bell (RIT Student) <pvb2147@ad.rit.edu>
2019-01-26 18:52:02 -0500

    Fixed thw swaping

--- Connect3/Assets/Scripts/ElliotScripts/cone3t Scripts/MySceneManager.cs
+++ Connect3/Assets/Scripts/ElliotScripts/cone3t Scripts/MySceneManager.cs
@@ -230,9 +230,11 @@ public class MySceneManager : MonoBehaviour {
 
             Vector3 pos1 = switchingArray[0].transform.position;
             Vector3 pos2 = switchingArray[1].transform.position;
-
-            switchingArray[0].transform.position = pos2;
-            switchingArray[1].transform.position = pos1;
+            if ((Mathf.Abs(pos1.x - pos2.x) < 2) && (Mathf.Abs(pos1.y - pos2.y) < 2))
+            {
+                switchingArray[0].transform.position = pos2;
+                switchingArray[1].transform.position = pos1;
+            }
 
             score += 10;
 
