3bf7ad9e3ddeacba2535b181c80be430944b1326
ebd1998 <ejp4604@rit.edu>
2019-01-28 09:52:23 -0500

    Added Green chips and started ease switching code

--- Connect3/Assets/Scripts/ElliotScripts/cone3t Scripts/MySceneManager.cs
+++ Connect3/Assets/Scripts/ElliotScripts/cone3t Scripts/MySceneManager.cs
@@ -7,20 +7,29 @@ public class MySceneManager : MonoBehaviour {
 
     public int numberOfMoves;
     public float score;
+
     public Material redMat;
     public Material blueMat;
+    public Material greenMat;
+
     public GameObject blankChip;
     public List<GameObject> chips;
+
     public GameObject[] switchingArray;
+    public bool switching;
+    float switchValue;
+    //int secondSwitchValue;
 
     private Vector3 screenPoint;
     private Vector3 offset;
 
 	// Use this for initialization
 	void Start () {
+        switching = false;
         chips = new List<GameObject>();
         numberOfMoves = 30;
         score = 0;
+        switchValue = .05f;
 
         switchingArray = new GameObject[2];
         
@@ -32,13 +41,15 @@ public class MySceneManager : MonoBehaviour {
         for(int x = 0; x < 5; x++)
         {
             // randomly chooses either 1 or 2
-            int randomNum = Random.Range(0, 2);
+            int randomNum = Random.Range(0, 3);
 
             // if 1; sets Chip identifier to 1 and gives it the red material
             if(randomNum == 1)
                 blankChip.GetComponent<Chips>().SetChip(1, redMat);
-            else // if 2; sets Chip identifier to 0 and gives it the blue material
+            else if(randomNum == 0)// if 0; sets Chip identifier to 0 and gives it the blue material
                 blankChip.GetComponent<Chips>().SetChip(0, blueMat);
+            else
+                blankChip.GetComponent<Chips>().SetChip(2, greenMat);
 
             // sets index in chips array
             blankChip.GetComponent<Chips>().index = x;
@@ -54,13 +65,15 @@ public class MySceneManager : MonoBehaviour {
         for (int x = 0; x < 5; x++)
         {
             // randomly chooses either 1 or 2
-            int randomNum = Random.Range(0, 2);
+            int randomNum = Random.Range(0, 3);
 
             // if 1; sets Chip identifier to 1 and gives it the red material
             if (randomNum == 1)
                 blankChip.GetComponent<Chips>().SetChip(1, redMat);
-            else // if 2; sets Chip identifier to 0 and gives it the blue material
+            else if (randomNum == 0)// if 0; sets Chip identifier to 0 and gives it the blue material
                 blankChip.GetComponent<Chips>().SetChip(0, blueMat);
+            else
+                blankChip.GetComponent<Chips>().SetChip(2, greenMat);
 
             // sets index in chips array
             blankChip.GetComponent<Chips>().index = 5 + x;
@@ -76,13 +89,15 @@ public class MySceneManager : MonoBehaviour {
         for (int x = 0; x < 5; x++)
         {
             // randomly chooses either 1 or 2
-            int randomNum = Random.Range(0, 2);
+            int randomNum = Random.Range(0, 3);
 
             // if 1; sets Chip identifier to 1 and gives it the red material
             if (randomNum == 1)
                 blankChip.GetComponent<Chips>().SetChip(1, redMat);
-            else // if 2; sets Chip identifier to 0 and gives it the blue material
+            else if (randomNum == 0)// if 0; sets Chip identifier to 0 and gives it the blue material
                 blankChip.GetComponent<Chips>().SetChip(0, blueMat);
+            else
+                blankChip.GetComponent<Chips>().SetChip(2, greenMat);
 
             // sets index in chips array
             blankChip.GetComponent<Chips>().index = 10 + x;
@@ -98,13 +113,15 @@ public class MySceneManager : MonoBehaviour {
         for (int x = 0; x < 5; x++)
         {
             // randomly chooses either 1 or 2
-            int randomNum = Random.Range(0, 2);
+            int randomNum = Random.Range(0, 3);
 
             // if 1; sets Chip identifier to 1 and gives it the red material
             if (randomNum == 1)
                 blankChip.GetComponent<Chips>().SetChip(1, redMat);
-            else // if 2; sets Chip identifier to 0 and gives it the blue material
+            else if (randomNum == 0)// if 0; sets Chip identifier to 0 and gives it the blue material
                 blankChip.GetComponent<Chips>().SetChip(0, blueMat);
+            else
+                blankChip.GetComponent<Chips>().SetChip(2, greenMat);
 
             // sets index in chips array
             blankChip.GetComponent<Chips>().index = 15 + x;
@@ -120,13 +137,15 @@ public class MySceneManager : MonoBehaviour {
         for (int x = 0; x < 5; x++)
         {
             // randomly chooses either 1 or 2
-            int randomNum = Random.Range(0, 2);
+            int randomNum = Random.Range(0, 3);
 
             // if 1; sets Chip identifier to 1 and gives it the red material
             if (randomNum == 1)
                 blankChip.GetComponent<Chips>().SetChip(1, redMat);
-            else // if 2; sets Chip identifier to 0 and gives it the blue material
+            else if (randomNum == 0)// if 0; sets Chip identifier to 0 and gives it the blue material
                 blankChip.GetComponent<Chips>().SetChip(0, blueMat);
+            else
+                blankChip.GetComponent<Chips>().SetChip(2, greenMat);
 
             // sets index in chips array
             blankChip.GetComponent<Chips>().index = 20 + x;
@@ -142,13 +161,15 @@ public class MySceneManager : MonoBehaviour {
         for (int x = 0; x < 5; x++)
         {
             // randomly chooses either 1 or 2
-            int randomNum = Random.Range(0, 2);
+            int randomNum = Random.Range(0, 3);
 
             // if 1; sets Chip identifier to 1 and gives it the red material
             if (randomNum == 1)
                 blankChip.GetComponent<Chips>().SetChip(1, redMat);
-            else // if 2; sets Chip identifier to 0 and gives it the blue material
+            else if (randomNum == 0)// if 0; sets Chip identifier to 0 and gives it the blue material
                 blankChip.GetComponent<Chips>().SetChip(0, blueMat);
+            else
+                blankChip.GetComponent<Chips>().SetChip(2, greenMat);
 
             // sets index in chips array
             blankChip.GetComponent<Chips>().index = 25 + x;
@@ -164,13 +185,15 @@ public class MySceneManager : MonoBehaviour {
         for (int x = 0; x < 5; x++)
         {
             // randomly chooses either 1 or 2
-            int randomNum = Random.Range(0, 2);
+            int randomNum = Random.Range(0, 3);
 
             // if 1; sets Chip identifier to 1 and gives it the red material
             if (randomNum == 1)
                 blankChip.GetComponent<Chips>().SetChip(1, redMat);
-            else // if 2; sets Chip identifier to 0 and gives it the blue material
+            else if (randomNum == 0)// if 0; sets Chip identifier to 0 and gives it the blue material
                 blankChip.GetComponent<Chips>().SetChip(0, blueMat);
+            else
+                blankChip.GetComponent<Chips>().SetChip(2, greenMat);
 
             // sets index in chips array
             blankChip.GetComponent<Chips>().index = 30 + x;
@@ -186,13 +209,15 @@ public class MySceneManager : MonoBehaviour {
         for (int x = 0; x < 5; x++)
         {
             // randomly chooses either 1 or 2
-            int randomNum = Random.Range(0, 2);
+            int randomNum = Random.Range(0, 3);
 
             // if 1; sets Chip identifier to 1 and gives it the red material
             if (randomNum == 1)
                 blankChip.GetComponent<Chips>().SetChip(1, redMat);
-            else // if 2; sets Chip identifier to 0 and gives it the blue material
+            else if (randomNum == 0)// if 0; sets Chip identifier to 0 and gives it the blue material
                 blankChip.GetComponent<Chips>().SetChip(0, blueMat);
+            else
+                blankChip.GetComponent<Chips>().SetChip(2, greenMat);
 
             // sets index in chips array
             blankChip.GetComponent<Chips>().index = 35 + x;
@@ -227,14 +252,54 @@ public class MySceneManager : MonoBehaviour {
             //{
 
             //}
+            switching = true;
 
             Vector3 pos1 = switchingArray[0].transform.position;
             Vector3 pos2 = switchingArray[1].transform.position;
 
+            int switchIndex = 0;
+
+            if (switching == true)
+            {
+                if (switchingArray[0].transform.position.x == switchingArray[1].transform.position.x)
+                    if (switchingArray[0].transform.position.y > switchingArray[1].transform.position.y)
+                        switchIndex = 1;
+                    else
+                        switchIndex = 2;
+                else if (switchingArray[0].transform.position.y == switchingArray[1].transform.position.y)
+                    if (switchingArray[0].transform.position.x < switchingArray[1].transform.position.x)
+                        switchIndex = 3;
+                    else
+                        switchIndex = 4;
+
+
+            }
+
+            if(switching)
+            {
+                if (switchIndex == 1)
+                {
+                    if (switchValue <= 1)
+                    {
+                        pos1.x -= .05f;
+                        pos2.x += .05f;
+                        switchValue += .05f;
+                    }
+                    else
+                    {
+                        switching = false;
+                        switchValue = .05f;
+                    }
+                        
+                }
+
+            }
+
+
             switchingArray[0].transform.position = pos2;
             switchingArray[1].transform.position = pos1;
 
-            score += 10;
+            // score += 10;
 
             for (int x = 0; x < 2; x++)
                 switchingArray[x] = null;
