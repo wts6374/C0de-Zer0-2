1f0ec86477f256770eeccf311cd0153e55018395
ejp4604 <ejp4604@g.rit.edu>
2019-01-30 13:16:04 -0500

    Rigged directional swap and chip movement together with the select pieces. Added a third color and populated grid for future use with matching. Added OnGUI element to dispay grid with index values. (FOR TESTING)

--- Connect3/Assets/Scripts/ElliotScripts/cone3t Scripts/MySceneManager.cs
+++ Connect3/Assets/Scripts/ElliotScripts/cone3t Scripts/MySceneManager.cs
@@ -7,206 +7,82 @@ public class MySceneManager : MonoBehaviour {
 
     public int numberOfMoves;
     public float score;
+
     public Material redMat;
     public Material blueMat;
+    public Material greenMat;
+
     public GameObject blankChip;
     public List<GameObject> chips;
+
     public GameObject[] switchingArray;
     public bool moved, moving;
-    private Vector3 Pos1, Pos2;
+    private Vector3 pos1, pos2;
     private int count;
 
-    private Vector3 screenPoint;
-    private Vector3 offset;
+    public GameObject[,] chipGrid;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         chips = new List<GameObject>();
         numberOfMoves = 30;
         score = count = 0;
         moved = moving = false;
-        Pos1 = Pos2 = Vector3.zero;
+        pos1 = pos2 = Vector3.zero;
+        chipGrid = new GameObject[8, 8];
         switchingArray = new GameObject[2];
+
+        int heightChange = 0;
+        float chipPlacement = -6.5f;
+        int indexSet = 0;
+        /*----------------------*/
+        #region chipSetUp
+        for (int x = 0; x < 8; x++)
+        {
+            heightChange = 0;
+            for (int y = 0; y < 8; y++)
+            {
+                // randomly chooses either 1 or 2
+                int randomNum = Random.Range(0, 3);
+
+                // if 1; sets Chip identifier to 1 and gives it the red material
+                if (randomNum == 1)
+                    blankChip.GetComponent<Chips>().SetChip(1, redMat);
+                else if (randomNum == 0)// if 0; sets Chip identifier to 0 and gives it the blue material
+                    blankChip.GetComponent<Chips>().SetChip(0, blueMat);
+                else
+                    blankChip.GetComponent<Chips>().SetChip(2, greenMat);
+
+                // sets index in chips array
+                blankChip.GetComponent<Chips>().index = indexSet;
+                
+
+                // instantiates Chip into scene
+                chips.Add(Instantiate(blankChip, new Vector3(chipPlacement, 1 + heightChange, 0), Quaternion.identity));
+
+                heightChange += 2;
+                chipGrid[x, y] = chips[indexSet];
+                indexSet++;
+            }
+
+            chipPlacement += 1.0f;
+        }
+
+       
+       
+        #endregion
+        /*----------------------*/
+
+        //for(int x = 0; x < 8; x++)
+        //{
+        //    for(int y = 0; y < 8; y++)
+        //    {
+        //        Debug.Log(chipGrid[x, y].GetComponent<Chips>().index);
+        //    }
+        //    Debug.Log("----------------");
+        //}
+
         
-        // REMOVE LATER
-        // value used to place chips next to each other for testing purposes
-        float chipPlacement = -3.5f;
-
-        //chips.Add(Instantiate(blankChip, new Vector3(chipPlacement, 1, 0), Quaternion.identity));
-
-        // creates 5 chips by default 
-        for (int x = 0; x < 5; x++)
-        {
-            // randomly chooses either 1 or 2
-            int randomNum = Random.Range(0, 2);
-
-            // if 1; sets Chip identifier to 1 and gives it the red material
-            if(randomNum == 1)
-                blankChip.GetComponent<Chips>().SetChip(1, redMat);
-            else // if 2; sets Chip identifier to 0 and gives it the blue material
-                blankChip.GetComponent<Chips>().SetChip(0, blueMat);
-
-            // sets index in chips array
-            blankChip.GetComponent<Chips>().index = x;
-
-            // instantiates Chip into scene
-            chips.Add(Instantiate(blankChip, new Vector3(chipPlacement, 1, 0), Quaternion.identity));
-            chipPlacement += 1.0f;
-        }
-
-        chipPlacement = -3.5f;
-
-        // creates 5 chips by default 
-        for (int x = 0; x < 5; x++)
-        {
-            // randomly chooses either 1 or 2
-            int randomNum = Random.Range(0, 2);
-
-            // if 1; sets Chip identifier to 1 and gives it the red material
-            if (randomNum == 1)
-                blankChip.GetComponent<Chips>().SetChip(1, redMat);
-            else // if 2; sets Chip identifier to 0 and gives it the blue material
-                blankChip.GetComponent<Chips>().SetChip(0, blueMat);
-
-            // sets index in chips array
-            blankChip.GetComponent<Chips>().index = 5 + x;
-
-            // instantiates Chip into scene
-            chips.Add(Instantiate(blankChip, new Vector3(chipPlacement, 3, 0), Quaternion.identity));
-            chipPlacement += 1.0f;
-        }
-
-        chipPlacement = -3.5f;
-
-        // creates 5 chips by default 
-        for (int x = 0; x < 5; x++)
-        {
-            // randomly chooses either 1 or 2
-            int randomNum = Random.Range(0, 2);
-
-            // if 1; sets Chip identifier to 1 and gives it the red material
-            if (randomNum == 1)
-                blankChip.GetComponent<Chips>().SetChip(1, redMat);
-            else // if 2; sets Chip identifier to 0 and gives it the blue material
-                blankChip.GetComponent<Chips>().SetChip(0, blueMat);
-
-            // sets index in chips array
-            blankChip.GetComponent<Chips>().index = 10 + x;
-
-            // instantiates Chip into scene
-            chips.Add(Instantiate(blankChip, new Vector3(chipPlacement, 5, 0), Quaternion.identity));
-            chipPlacement += 1.0f;
-        }
-
-        chipPlacement = -3.5f;
-
-        // creates 5 chips by default 
-        for (int x = 0; x < 5; x++)
-        {
-            // randomly chooses either 1 or 2
-            int randomNum = Random.Range(0, 2);
-
-            // if 1; sets Chip identifier to 1 and gives it the red material
-            if (randomNum == 1)
-                blankChip.GetComponent<Chips>().SetChip(1, redMat);
-            else // if 2; sets Chip identifier to 0 and gives it the blue material
-                blankChip.GetComponent<Chips>().SetChip(0, blueMat);
-
-            // sets index in chips array
-            blankChip.GetComponent<Chips>().index = 15 + x;
-
-            // instantiates Chip into scene
-            chips.Add(Instantiate(blankChip, new Vector3(chipPlacement, 7, 0), Quaternion.identity));
-            chipPlacement += 1.0f;
-        }
-
-        chipPlacement = -3.5f;
-
-        // creates 5 chips by default 
-        for (int x = 0; x < 5; x++)
-        {
-            // randomly chooses either 1 or 2
-            int randomNum = Random.Range(0, 2);
-
-            // if 1; sets Chip identifier to 1 and gives it the red material
-            if (randomNum == 1)
-                blankChip.GetComponent<Chips>().SetChip(1, redMat);
-            else // if 2; sets Chip identifier to 0 and gives it the blue material
-                blankChip.GetComponent<Chips>().SetChip(0, blueMat);
-
-            // sets index in chips array
-            blankChip.GetComponent<Chips>().index = 20 + x;
-
-            // instantiates Chip into scene
-            chips.Add(Instantiate(blankChip, new Vector3(chipPlacement, 9, 0), Quaternion.identity));
-            chipPlacement += 1.0f;
-        }
-
-        chipPlacement = -3.5f;
-
-        // creates 5 chips by default 
-        for (int x = 0; x < 5; x++)
-        {
-            // randomly chooses either 1 or 2
-            int randomNum = Random.Range(0, 2);
-
-            // if 1; sets Chip identifier to 1 and gives it the red material
-            if (randomNum == 1)
-                blankChip.GetComponent<Chips>().SetChip(1, redMat);
-            else // if 2; sets Chip identifier to 0 and gives it the blue material
-                blankChip.GetComponent<Chips>().SetChip(0, blueMat);
-
-            // sets index in chips array
-            blankChip.GetComponent<Chips>().index = 25 + x;
-
-            // instantiates Chip into scene
-            chips.Add(Instantiate(blankChip, new Vector3(chipPlacement, 11, 0), Quaternion.identity));
-            chipPlacement += 1.0f;
-        }
-
-        chipPlacement = -3.5f;
-
-        // creates 5 chips by default 
-        for (int x = 0; x < 5; x++)
-        {
-            // randomly chooses either 1 or 2
-            int randomNum = Random.Range(0, 2);
-
-            // if 1; sets Chip identifier to 1 and gives it the red material
-            if (randomNum == 1)
-                blankChip.GetComponent<Chips>().SetChip(1, redMat);
-            else // if 2; sets Chip identifier to 0 and gives it the blue material
-                blankChip.GetComponent<Chips>().SetChip(0, blueMat);
-
-            // sets index in chips array
-            blankChip.GetComponent<Chips>().index = 30 + x;
-
-            // instantiates Chip into scene
-            chips.Add(Instantiate(blankChip, new Vector3(chipPlacement, 13, 0), Quaternion.identity));
-            chipPlacement += 1.0f;
-        }
-
-        chipPlacement = -3.5f;
-
-        // creates 5 chips by default 
-        for (int x = 0; x < 5; x++)
-        {
-            // randomly chooses either 1 or 2
-            int randomNum = Random.Range(0, 2);
-
-            // if 1; sets Chip identifier to 1 and gives it the red material
-            if (randomNum == 1)
-                blankChip.GetComponent<Chips>().SetChip(1, redMat);
-            else // if 2; sets Chip identifier to 0 and gives it the blue material
-                blankChip.GetComponent<Chips>().SetChip(0, blueMat);
-
-            // sets index in chips array
-            blankChip.GetComponent<Chips>().index = 35 + x;
-
-            // instantiates Chip into scene
-            chips.Add(Instantiate(blankChip, new Vector3(chipPlacement, 15, 0), Quaternion.identity));
-            chipPlacement += 1.0f;
-        }
     }
 	
 	// Update is called once per frame
@@ -217,7 +93,9 @@ public class MySceneManager : MonoBehaviour {
             EndGame();
 
         HandleSwitching();
-	}
+        //Debug.Log(chipGrid[0, 2].GetComponent<Chips>().index);
+        //Debug.Log(chipGrid[0, 2].transform.position);
+    }
 
     void EndGame()
     {
@@ -227,15 +105,26 @@ public class MySceneManager : MonoBehaviour {
     //Updated Switching
     void HandleSwitching()
     {
+        
         //if tow elements are selected for switching 
-        if(switchingArray[1] != null)
+        if (switchingArray[1] != null)
         {
+            
+
             //And not in the process of switching, store the end positions
             if (!moving)
             {
-                Pos1 = switchingArray[0].transform.position;
-                Pos2 = switchingArray[1].transform.position;
-                moving = true;
+                pos1 = switchingArray[0].transform.position;
+                pos2 = switchingArray[1].transform.position;
+                if ((Mathf.Abs(pos1.x - pos2.x) < 2) && (Mathf.Abs(pos1.y - pos2.y) < 0.5) || (Mathf.Abs(pos1.x - pos2.x) < 0.5) && (Mathf.Abs(pos1.y - pos2.y) < 2))
+                {
+                    moving = true;
+                }
+                else
+                {
+                    switchingArray = new GameObject[2];
+                    return;
+                }
             }
             else
             {
@@ -243,17 +132,42 @@ public class MySceneManager : MonoBehaviour {
                 if (count < 10)
                 {
                     //Slowly move between the positions and add to count
-                    switchingArray[0].transform.position += (Pos2 - switchingArray[0].transform.position) / 2f;
-                    switchingArray[1].transform.position += (Pos1 - switchingArray[1].transform.position) / 2f;
+                    //switchingArray[0].transform.position += (Pos2 - switchingArray[0].transform.position) / 2f;
+                    //switchingArray[1].transform.position += (Pos1 - switchingArray[1].transform.position) / 2f;
+
+                    Vector3 finalSwitch1 = (pos2 - switchingArray[0].transform.position) / 2f;
+                    Vector3 finalSwitch2 = (pos1 - switchingArray[1].transform.position) / 2f;
+
+                    // changes z value to 0 to prevent chips from falling off
+                    // when the switching vectors are added to the switching chips
+                    finalSwitch1.z = 0;
+                    finalSwitch2.z = 0;
+
+                    // adds values for switching objects
+                    switchingArray[0].transform.position += finalSwitch1;
+                    switchingArray[1].transform.position += finalSwitch2;
+
                     count++;
+
+                    // removes rigidbodies to prevent switching from affecting the rest of the chips
+                    // when they move
+                    for(int x = 0; x < chips.Count; x++)
+                        Destroy(chips[x].GetComponent<Rigidbody>());
+                    
                 }
                 else
                 {
+
                     //reset the empty array and count while adding to score
                     moving = false;
+                    SwitchGridPos(switchingArray[0].GetComponent<Chips>().index, switchingArray[1].GetComponent<Chips>().index);
                     switchingArray = new GameObject[2];
                     count = 0;
                     score += 10;
+
+                    // adds rigidbodies again 
+                    for (int x = 0; x < chips.Count; x++)
+                        chips[x].AddComponent<Rigidbody>();
                 }
             }
             
@@ -262,5 +176,70 @@ public class MySceneManager : MonoBehaviour {
 
 
         
+    }
+
+    void SwitchGridPos(int index1, int index2)
+    {
+        GameObject temp1 = null;
+        GameObject temp2 = null;
+
+        int x1 = -100;
+        int y1 = -100;
+
+        int x2 = -100;
+        int y2 = -100;
+
+        for(int x = 0; x < 8; x++)
+        {
+            for(int y = 0; y < 8; y++)
+            {
+                if(temp1 == null && chipGrid[x,y].GetComponent<Chips>().index == index1)
+                {
+                    x1 = x;
+                    y1 = y;
+                    temp1 = chipGrid[x, y];
+                }
+                else if( temp2 == null && chipGrid[x, y].GetComponent<Chips>().index == index2)
+                {
+                    
+                    x2 = x;
+                    y2 = y;
+                    temp2 = chipGrid[x, y];
+                }
+
+                //int c = chipGrid[x, y].GetComponent<Chips>().index;
+                //Debug.Log(c);
+                //else
+                //{
+                //    break;
+                //}
+            }
+            if(temp1 != null && temp2 != null)
+            {
+                break;
+            }
+        }
+        chipGrid[x1, y1] = temp2;
+        chipGrid[x2, y2] = temp1;
+
+    }
+
+    private void OnGUI()
+    {
+
+        string grid = null;
+        for(int x = 7; x > -1; x--)
+        {
+            for(int y = 0; y < 8; y++)
+            {
+                if(chipGrid[y, x].GetComponent<Chips>().index < 10)
+                    grid += "|" + chipGrid[y, x].GetComponent<Chips>().index + " |";
+                else
+                    grid += "|" + chipGrid[y, x].GetComponent<Chips>().index + "|";
+            }
+            grid += "\n";
+        }
+
+        GUI.Box(new Rect(450, 10, 200, 150), grid);
     }
 }
