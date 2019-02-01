b45fb1bfc0623ba8a8f8eeadb6d145489b72ed4a
wts6374 <wts6374@g.rit.edu>
2019-02-01 15:12:13 -0500

    All right so I got the chips to have only a sphere collider and adjusted the rigidbody accordingly, had to script in to have the rotation and position freeze again after they switch, I re-textured the grid so it's more reminisent of Connect 4 and I fixed  a glitch where the game would essentially soft lock if you choose the same chip twice.

--- Connect3/Assets/Scripts/ElliotScripts/cone3t Scripts/MySceneManager.cs
+++ Connect3/Assets/Scripts/ElliotScripts/cone3t Scripts/MySceneManager.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MySceneManager : MonoBehaviour {
+public class MySceneManager : MonoBehaviour
+{
 
     public int numberOfMoves;
     public float score;
@@ -23,7 +24,8 @@ public class MySceneManager : MonoBehaviour {
     public GameObject[,] chipGrid;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         chips = new List<GameObject>();
         numberOfMoves = 30;
         score = count = 0;
@@ -55,7 +57,7 @@ public class MySceneManager : MonoBehaviour {
 
                 // sets index in chips array
                 blankChip.GetComponent<Chips>().index = indexSet;
-                
+
 
                 // instantiates Chip into scene
                 chips.Add(Instantiate(blankChip, new Vector3(chipPlacement, 1 + heightChange, 0), Quaternion.identity));
@@ -68,8 +70,8 @@ public class MySceneManager : MonoBehaviour {
             chipPlacement += 1.0f;
         }
 
-       
-       
+
+
         #endregion
         /*----------------------*/
 
@@ -82,11 +84,12 @@ public class MySceneManager : MonoBehaviour {
         //    Debug.Log("----------------");
         //}
 
-        
+
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         if (Input.GetMouseButtonDown(0))
             numberOfMoves--;
         if (numberOfMoves == 0)
@@ -95,6 +98,17 @@ public class MySceneManager : MonoBehaviour {
         HandleSwitching();
         //Debug.Log(chipGrid[0, 2].GetComponent<Chips>().index);
         //Debug.Log(chipGrid[0, 2].transform.position);
+
+        //This should make sure all the chips don't unfreeze in position and rotation after they move
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                chipGrid[i, j].gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+            }
+        }
+
+
     }
 
     void EndGame()
@@ -105,77 +119,84 @@ public class MySceneManager : MonoBehaviour {
     //Updated Switching
     void HandleSwitching()
     {
-        
-        //if tow elements are selected for switching 
+
+        //if two elements are selected for switching 
         if (switchingArray[1] != null)
         {
-            
-
-            //And not in the process of switching, store the end positions
-            if (!moving)
+            //Unselects a position if they are at the same point
+            if (switchingArray[0] == switchingArray[1])
             {
-                pos1 = switchingArray[0].transform.position;
-                pos2 = switchingArray[1].transform.position;
-                if ((Mathf.Abs(pos1.x - pos2.x) < 1.5) && (Mathf.Abs(pos1.y - pos2.y) < 0.5) || (Mathf.Abs(pos1.x - pos2.x) < 0.5) && (Mathf.Abs(pos1.y - pos2.y) < 1.5))
-                {
-                    moving = true;
-                }
-                else
-                {
-                    switchingArray = new GameObject[2];
-                    return;
-                }
+                switchingArray[0] = null;
+                switchingArray[1] = null;
             }
             else
             {
-                //only move a certain amount as not to get stuck
-                if (count < 10)
+                //And not in the process of switching, store the end positions
+                if (!moving)
                 {
-                    //Slowly move between the positions and add to count
-                    //switchingArray[0].transform.position += (Pos2 - switchingArray[0].transform.position) / 2f;
-                    //switchingArray[1].transform.position += (Pos1 - switchingArray[1].transform.position) / 2f;
-
-                    Vector3 finalSwitch1 = (pos2 - switchingArray[0].transform.position) / 2f;
-                    Vector3 finalSwitch2 = (pos1 - switchingArray[1].transform.position) / 2f;
-
-                    // changes z value to 0 to prevent chips from falling off
-                    // when the switching vectors are added to the switching chips
-                    finalSwitch1.z = 0;
-                    finalSwitch2.z = 0;
-
-                    // adds values for switching objects
-                    switchingArray[0].transform.position += finalSwitch1;
-                    switchingArray[1].transform.position += finalSwitch2;
-
-                    count++;
-
-                    // removes rigidbodies to prevent switching from affecting the rest of the chips
-                    // when they move
-                    for(int x = 0; x < chips.Count; x++)
-                        Destroy(chips[x].GetComponent<Rigidbody>());
-                    
+                    pos1 = switchingArray[0].transform.position;
+                    pos2 = switchingArray[1].transform.position;
+                    if ((Mathf.Abs(pos1.x - pos2.x) < 1.5) && (Mathf.Abs(pos1.y - pos2.y) < 0.5) || (Mathf.Abs(pos1.x - pos2.x) < 0.5) && (Mathf.Abs(pos1.y - pos2.y) < 1.5))
+                    {
+                        moving = true;
+                    }
+                    else
+                    {
+                        switchingArray = new GameObject[2];
+                        return;
+                    }
                 }
                 else
                 {
+                    //only move a certain amount as not to get stuck
+                    if (count < 10)
+                    {
+                        //Slowly move between the positions and add to count
+                        //switchingArray[0].transform.position += (Pos2 - switchingArray[0].transform.position) / 2f;
+                        //switchingArray[1].transform.position += (Pos1 - switchingArray[1].transform.position) / 2f;
 
-                    //reset the empty array and count while adding to score
-                    moving = false;
-                    SwitchGridPos(switchingArray[0].GetComponent<Chips>().index, switchingArray[1].GetComponent<Chips>().index);
-                    switchingArray = new GameObject[2];
-                    count = 0;
-                    score += 10;
+                        Vector3 finalSwitch1 = (pos2 - switchingArray[0].transform.position) / 2f;
+                        Vector3 finalSwitch2 = (pos1 - switchingArray[1].transform.position) / 2f;
 
-                    // adds rigidbodies again 
-                    for (int x = 0; x < chips.Count; x++)
-                        chips[x].AddComponent<Rigidbody>();
+                        // changes z value to 0 to prevent chips from falling off
+                        // when the switching vectors are added to the switching chips
+                        finalSwitch1.z = 0;
+                        finalSwitch2.z = 0;
+
+                        // adds values for switching objects
+                        switchingArray[0].transform.position += finalSwitch1;
+                        switchingArray[1].transform.position += finalSwitch2;
+
+                        count++;
+
+                        // removes rigidbodies to prevent switching from affecting the rest of the chips
+                        // when they move
+                        for (int x = 0; x < chips.Count; x++)
+                            Destroy(chips[x].GetComponent<Rigidbody>());
+
+                    }
+                    else
+                    {
+
+                        //reset the empty array and count while adding to score
+                        moving = false;
+                        SwitchGridPos(switchingArray[0].GetComponent<Chips>().index, switchingArray[1].GetComponent<Chips>().index);
+                        switchingArray = new GameObject[2];
+                        count = 0;
+                        score += 10;
+
+                        // adds rigidbodies again 
+                        for (int x = 0; x < chips.Count; x++)
+                            chips[x].AddComponent<Rigidbody>();
+                    }
                 }
             }
-            
+
         }
 
 
 
-        
+
     }
 
     void SwitchGridPos(int index1, int index2)
@@ -189,19 +210,19 @@ public class MySceneManager : MonoBehaviour {
         int x2 = -100;
         int y2 = -100;
 
-        for(int x = 0; x < 8; x++)
+        for (int x = 0; x < 8; x++)
         {
-            for(int y = 0; y < 8; y++)
+            for (int y = 0; y < 8; y++)
             {
-                if(temp1 == null && chipGrid[x,y].GetComponent<Chips>().index == index1)
+                if (temp1 == null && chipGrid[x, y].GetComponent<Chips>().index == index1)
                 {
                     x1 = x;
                     y1 = y;
                     temp1 = chipGrid[x, y];
                 }
-                else if( temp2 == null && chipGrid[x, y].GetComponent<Chips>().index == index2)
+                else if (temp2 == null && chipGrid[x, y].GetComponent<Chips>().index == index2)
                 {
-                    
+
                     x2 = x;
                     y2 = y;
                     temp2 = chipGrid[x, y];
@@ -214,7 +235,7 @@ public class MySceneManager : MonoBehaviour {
                 //    break;
                 //}
             }
-            if(temp1 != null && temp2 != null)
+            if (temp1 != null && temp2 != null)
             {
                 break;
             }
@@ -228,11 +249,11 @@ public class MySceneManager : MonoBehaviour {
     {
 
         string grid = null;
-        for(int x = 7; x > -1; x--)
+        for (int x = 7; x > -1; x--)
         {
-            for(int y = 0; y < 8; y++)
+            for (int y = 0; y < 8; y++)
             {
-                if(chipGrid[y, x].GetComponent<Chips>().index < 10)
+                if (chipGrid[y, x].GetComponent<Chips>().index < 10)
                     grid += "|" + chipGrid[y, x].GetComponent<Chips>().index + " |";
                 else
                     grid += "|" + chipGrid[y, x].GetComponent<Chips>().index + "|";
